c052865abb595bb44539925626e97b709c9cc1b6
unixkarma <nicokernelpanic@gmail.com>
2024-07-27 09:17:57 -0500

    All Good

--- laboratorio-8/pokedex/urls.py
+++ laboratorio-8/pokedex/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"),
     path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
-    path("login/", views.CustomLoginView.as_view(), name='login')
+    path("edit_pokemon/<int:id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("login/", views.CustomLoginView.as_view(), name='login'),
+    path("delete_pokemon/<int:id>/", views.delete_pokemon, name="delete_pokemon"),
+
 ]
--- laboratorio-8/pokedex/views.py
+++ laboratorio-8/pokedex/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.template import loader
 from .models import Pokemon
 from django.contrib.auth.views import LoginView
@@ -12,7 +12,7 @@ def index(request):
     return HttpResponse(template.render({'pokemons': pokemons}, request))
 
 def pokemon(request, pokemon_id):
-    pokemon = Pokemon.objects.get(pk = pokemon_id)
+    pokemon = get_object_or_404(Pokemon, pk=pokemon_id)
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
@@ -28,8 +28,30 @@ def add_pokemon(request):
     else:
         form = PokemonForm()
 
-    return render(request, 'add_pokemon.html', {'form':form})
+    return render(request, 'pokemon_form.html', {'form':form})
 
 
 class CustomLoginView(LoginView):
     template_name="login.html"
+
+
+@login_required
+def edit_pokemon(request, id):
+    pokemon = get_object_or_404(Pokemon, pk=id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+        
+
+    return render(request, 'pokemon_form.html', {'form':form})
+
+
+@login_required
+def delete_pokemon(request, id):
+    pokemon = get_object_or_404(Pokemon, pk=id)
+    pokemon.delete()
+    return redirect("pokedex:index")
